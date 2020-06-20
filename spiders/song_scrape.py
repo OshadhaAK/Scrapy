b0968b258502df68f0fa215770d1847bf9237d08
oshadhaAK <oshadhakatulanda.16@cse.mrt.ac.lk>
2020-06-21 00:02:23 +0530

    updated scrapy.py

--- spiders/song_scrape.py
+++ spiders/song_scrape.py
@@ -1,12 +1,11 @@
 import sys
 sys.path.append(".")
 import scrapy
-from deranafm.items import DeranafmItem
+from items import DeranafmItem
 
 class Song(scrapy.Spider):
     name = "song_scraper"
 
-    # First Start Url
     start_urls = ["http://www.fmderana.lk/sinhala-music-videos"]
 
     npages = 2
@@ -16,41 +15,23 @@ class Song(scrapy.Spider):
         start_urls.append("http://www.fmderana.lk/sinhala-music-videos/page/" + str(i) + "")
 
     def parse(self, response):
-        for href in response.xpath("//div[contains(@class, 'item col-6 col-sm-4 col-md-3 col-lg-4 col-xl-3')]/article[contains(@class, 'radio-item artist')]/figure/a//@href"):
-            # add the scheme, eg http://
+        for href in response.xpath("//div[contains(@class, 'item col-6 col-sm-4 col-md-3 col-lg-4 col-xl-3')]/article[contains(@class, 'radio-item artist')]/figure/a[contains(@class, 'inner-click')]//@href"):
             url = "http:" + href.extract()
-
             yield scrapy.Request(url, callback=self.parse_dir_contents)
+
 
     def parse_dir_contents(self, response):
         item = DeranafmItem()
 
-        # Getting Campaign Title
-        item['song'] = response.xpath("//div[contains(@class, 'video-content')]/h1/descendant::text()").extract()[0].strip()
-
-        # Getting Amount Raised
-        item['mainArtist'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Main Artist')]/following-sibling::dd[1]/descendant::text()").extract()[0]
-
-        # Goal
-        item['music'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Music')]/following-sibling::dd[1]/text()").extract()[0]
-        
-        # Currency Type (US Dollar Etc)
-        item['lyrics'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Lyrics')]/following-sibling::dd[1]/text()").extract()[0]
-        
-        # Campaign End (Month year etc)
-        item['visits'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Visits')]/following-sibling::dd[1]/text()").extract()[0]
-        
-        # Number of contributors
-        item['downloads'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Downloads')]/following-sibling::dd[1]/text()").extract()[0]
-
-        item['videoURI'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Music')]/following-sibling::dd[1]/text()").extract()[0]
-
-        # Getting Story
-        # downloadable_formats_list = response.xpath("//div[contains(@id, 'full-story')]/descendant::text()").extract()
-        # downloadable_formats_list = [x.strip() for x in downloadable_formats_list if len(x.strip()) > 0]
-        # item['downloadFormats'] = " ".join(downloadable_formats_list)
-
-        # Url (The link to the page)
+        item['song'] = response.xpath("//div[contains(@class, 'video-content')]/h1/text()").extract()[0].strip()
+        item['mainArtist'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Main Artist')]/following-sibling::dd[1]/text()").extract()[0].strip()
+        item['music'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Music')]/following-sibling::dd[1]/text()").extract()[0].strip()
+        item['lyrics'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Lyrics')]/following-sibling::dd[1]/text()").extract()[0].strip()
+        item['visits'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Visits')]/following-sibling::dd[1]/text()").extract()[0].strip()
+        item['downloads'] = response.xpath("//div[contains(@class, 'video-extra-info')]/dl[contains(@class, 'details-list')]/dt[contains(text(), 'Downloads')]/following-sibling::dd[1]/text()").extract()[0].strip()
+        for j in response.xpath("//div[contains(@class, 'video-icons')]/ul[contains(@class, 'icons-list')]/li/a[contains(@class, 'btn btn-default btn-action btn-download')]//@href").extract():
+            item['downloadFormats'] += [j.strip().split("=")[-1]]
+        item['video'] = response.xpath("").extract()[0].strip()
         item['url'] = response.xpath("//meta[@property='og:url']/@content").extract()
 
         yield item
